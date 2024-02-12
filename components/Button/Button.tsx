2975f0d9f3d2e9e2d201ec0678ae55a397b6e4ed
francisco.barbosa <francisco.barbosa@mv.com.br>
2024-02-11 22:42:23 -0300

    creating first storiebook to button component

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,11 +1,11 @@
-type ButtonProps = {
+export type ButtonProps = {
   children: React.ReactNode;
 } & React.ButtonHTMLAttributes<HTMLButtonElement>;
 
 export function Button({ children, className }: ButtonProps) {
   return (
     <button
-      className={`bg-[#2D5BFF] ROUNDED-[8px] px-[32px] py-[12px] text-white ${className}`}
+      className={`bg-[#2D5BFF] rounded-[8px] px-[32px] py-[12px] text-white ${className}`}
     >
       {children}
     </button>
