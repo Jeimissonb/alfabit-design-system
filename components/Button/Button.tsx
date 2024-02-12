7ab46f76081b0cd2d6206fe774bb17b59535f74d
Jeimissonb <jeimissonb@hotmail.com>
2024-02-12 12:09:54 -0300

    design tokens of spacings and fonts

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -5,7 +5,7 @@ export type ButtonProps = {
 export function Button({ children, className }: ButtonProps) {
   return (
     <button
-      className={`bg-primary rounded-[8px] px-[32px] py-[12px] text-white ${className}`}
+      className={`bg-primary rounded-md px-[32px] py-[12px] text-white ${className}`}
     >
       {children}
     </button>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,11 +12,43 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    fontFamily: {
+      mulish: "Mulish, sans-serif",
+    },
     screens: {
       mobile: "var(--screen-mobile)",
       tablet: "var(--screen-tablet)",
       desktop: "var(--screen-desktop)",
       tv: "var(--screen-tv)",
+    },
+    spacing: {
+      none: "var(--spacing-none) /* 0px */",
+      "4xs": "var(--spacing-4xs) /* 8px */",
+      "2xs": "var(--spacing-2xs) /* 12px */",
+      xs: "var(--spacing-xs) /* 16px */",
+      sm: "var(--spacing-sm) /* 20px */",
+      md: "var(--spacing-md) /* 24px */",
+      lg: "var(--spacing-lg) /* 32px */",
+      xl: "var(--spacing-xl) /* 40px */",
+      "2xl": "var(--spacing-2xl) /* 48px */",
+      "4xl": "var(--spacing-4xl) /* 56px */",
+      0: "var(--spacing-none) /* 0px */",
+      1: "var(--spacing-4xs) /* 8px */",
+      2: "var(--spacing-2xs) /* 12px */",
+      3: "var(--spacing-xs) /* 16px */",
+      4: "var(--spacing-sm) /* 20px */",
+      5: "var(--spacing-md) /* 24px */",
+      6: "var(--spacing-lg) /* 32px */",
+      7: "var(--spacing-xl) /* 40px */",
+      8: "var(--spacing-2xl) /* 48px */",
+      9: "var(--spacing-4xl) /* 56px */",
+    },
+    fontSize: {
+      xs: "var(--font-xs)",
+      sm: "var(--font-sm)",
+      md: "var(--font-md)",
+      lg: "var(--font-lg)",
+      xl: "var(--font-xl)",
     },
     extend: {
       colors: {
@@ -47,6 +79,12 @@ const config: Config = {
       },
       blur: {
         default: "var(--blur)",
+      },
+      borderRadius: {
+        none: "var(--border-radius-none)",
+        sm: "var(--border-radius-sm)",
+        md: "var(--border-radius-md)",
+        lg: "var(--border-radius-lg)",
       },
     },
   },
