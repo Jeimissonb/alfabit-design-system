0a9b3b67d3011450ac2942bf21b377ca39d5c6de
jeimissonb <jeimissonb@hotmail.com>
2024-02-12 03:23:47 -0300

    improvments on design token of colors, add screens, box shadows and blur of design tokens

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,10 @@
 import type { Config } from "tailwindcss";
 
+function toRgba(cssVariable: string) {
+  const color = `var(${cssVariable})`;
+  return ({ opacityValue }: any) => `rgba(${color}, ${opacityValue})`;
+}
+
 const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -7,9 +12,41 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      mobile: "var(--screen-mobile)",
+      tablet: "var(--screen-tablet)",
+      desktop: "var(--screen-desktop)",
+      tv: "var(--screen-tv)",
+    },
     extend: {
       colors: {
-        primary: ({ opacityValue }) => `rgba(var(--primary), ${opacityValue})`,
+        primary: toRgba("--primary"),
+        secondary: toRgba("--secondary"),
+        tertiary: toRgba("--tertiary"),
+        quaternary: toRgba("--quaternary"),
+        hover: toRgba("--hover"),
+        click: toRgba("--click"),
+        icon: toRgba("--icon"),
+        outline: toRgba("--outline"),
+        divider: toRgba("--divider"),
+        "bg-light": toRgba("--bg-light"),
+        "bg-dark": toRgba("--bg-dark"),
+        "bg-disabled": toRgba("--bg-disabled"),
+        "text-primary": toRgba("--text-primary"),
+        "text-secondary": toRgba("--text-secondary"),
+        "text-tertiary": toRgba("--text-tertiary"),
+        "text-disabled": toRgba("--text-disabled"),
+      },
+      boxShadow: {
+        sm: "var(--shadow-sm)",
+        md: "var(--shadow-md)",
+        lg: "var(--shadow-lg)",
+        focus: "var(--shadow-focus)",
+        outline: "var(--shadow-outline)",
+        "button-focus": "var(--shadow-button-focus)",
+      },
+      blur: {
+        default: "var(--blur)",
       },
     },
   },
