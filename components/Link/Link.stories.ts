9e33bb10b0c37c0f45ab59fe2daa499116db8d47
Jeimissonb <jeimissonb@hotmail.com>
2024-02-12 23:56:56 -0300

    finish link component and storybook doc

--- components/Link/Link.stories.ts
+++ components/Link/Link.stories.ts
@@ -4,9 +4,21 @@ import { Link, LinkProps } from "./Link";
 
 const meta: Meta<LinkProps> = {
   title: "Molecules/Link",
-  tags: ["autodocs"],
   component: Link,
-  argTypes: {},
+  argTypes: {
+    children: {
+      type: "string",
+    },
+    href: {
+      type: "string",
+    },
+    disabled: {
+      type: "boolean",
+    },
+    className: {
+      type: "string",
+    },
+  },
 };
 
 export default meta;
@@ -15,5 +27,21 @@ export const Primary: StoryObj<LinkProps> = {
   args: {
     children: "Link",
     href: "/",
+  },
+};
+
+export const Disabled: StoryObj<LinkProps> = {
+  args: {
+    children: "Link",
+    href: "/",
+    disabled: true,
+  },
+};
+
+export const Violet: StoryObj<LinkProps> = {
+  args: {
+    children: "Link",
+    href: "/",
+    className: "theme-violet",
   },
 };
