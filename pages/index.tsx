88ae86fdd2a2188cbb9c9e0ac394a0bafdb6e912
Jeimissonb <jeimissonb@hotmail.com>
2024-02-14 21:21:30 -0300

    creating VITE project and add TailwindCss

--- pages/index.tsx
+++
@@ -1,5 +0,0 @@
-import { Button } from "@/components/Button/Button";
-
-export default function App() {
-  return <Button> Botão </Button>;
-}
---
+++ src/App.tsx
@@ -0,0 +1,3 @@
+export function App() {
+  return <h1 className="font-bold">Hello world</h1>;
+}
