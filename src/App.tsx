629ced27c046723bd8178c224b20fb3d89f73538
Jeimissonb <jeimissonb@hotmail.com>
2024-02-14 21:53:40 -0300

    configs of design tokens of tailwind, custom properties added on tailwind.config, font Mulish added to project and delete storybook useless files

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,7 @@
 export function App() {
-  return <h1 className="font-bold">Hello world</h1>;
+  return (
+    <div className="bg-emerald-100 w-screen h-screen text-center font-bold text-4xl">
+      <h1>You should run on terminal: npm run storybook</h1>
+    </div>
+  );
 }
