91bcac7a26be69255e96d8d618ad6822e009ccab
Jeimissonb <jeimissonb@hotmail.com>
2024-02-15 11:51:53 -0300

    Updates

--- assets/preview-BHcRUdWU.js
+++
@@ -1,7 +0,0 @@
-import{_ as e}from"./iframe-DRQ9XOl3.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-jCTzHcrl.js"),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9]),import.meta.url);return new r}}};export{a as parameters};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-jCTzHcrl.js","./iframe-DRQ9XOl3.js","./index-DH5ua8nC.js","./_commonjsHelpers-Cpj98o6Y.js","./react-18-iZ4baTDS.js","./index-D9uilScg.js","./index-CmB2LsNu.js","./index-Bw8VTzHM.js","./index-UvUotFBn.js","./index-DrFu-skq.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
