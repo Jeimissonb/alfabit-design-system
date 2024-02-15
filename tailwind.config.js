8d42a464dc8277271b99d1f8387c7e5f2b03f9cc
Jeimissonb <jeimissonb@hotmail.com>
2024-02-14 22:10:31 -0300

    add input component

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "../components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
     fontFamily: {
       mulish: "Mulish, sans-serif",
