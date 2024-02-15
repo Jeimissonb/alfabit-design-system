dff1500a3f57feeb42a36f228bd078cbbf26c3b2
Jeimissonb <jeimissonb@hotmail.com>
2024-02-15 10:41:58 -0300

    Updates

--- assets/preview-B20643xA.js
+++
@@ -1,7 +0,0 @@
-import{_ as e}from"./iframe-CLVq4hVb.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-MU3kBFRt.js"),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9]));return new r}}};export{a as parameters};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["assets/DocsRenderer-NNNQARDV-MU3kBFRt.js","assets/iframe-CLVq4hVb.js","assets/index-DH5ua8nC.js","assets/_commonjsHelpers-Cpj98o6Y.js","assets/react-18-iZ4baTDS.js","assets/index-D9uilScg.js","assets/index-D-yWupOV.js","assets/index-Bw8VTzHM.js","assets/index-UvUotFBn.js","assets/index-DrFu-skq.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
